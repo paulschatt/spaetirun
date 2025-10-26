2168c6a7d6301d2da6784443ee0b8dbbf2e0121a
Paul <paul.schatt@company-shield.de>
2025-10-26 22:21:24 +0100

    Create App

---
+++ src/main/java/spaetirun/SpaetirunApplication.java
@@ -0,0 +1,11 @@
+package spaetirun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaetirunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(spaetirun.SpaetirunApplication.class, args);
+    }
+}
