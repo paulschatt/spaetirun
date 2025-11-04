32a575cae410bc66ad31bd8a46cc6627b296b088
Paul <paul.schatt@company-shield.de>
2025-11-04 13:54:59 +0100

    use required args constructor

--- src/main/java/spaetirun/team/service/TeamService.java
+++ src/main/java/spaetirun/team/service/TeamService.java
@@ -1,6 +1,7 @@
 package spaetirun.team.service;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,10 @@ import spaetirun.team.repository.TeamRepository;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class TeamService {
 
-    @Autowired
-    private TeamRepository teamRepository;
+    private final TeamRepository teamRepository;
 
     @GetMapping("/teams")
     public List<Team> getAllTeams() {
